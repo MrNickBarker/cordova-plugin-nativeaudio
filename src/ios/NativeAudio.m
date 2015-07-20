964338dca766f6d7e3ca4b9b0fcf70a35908ca11
Nikola Lajic <n.lajic@gmail.com>
2015-07-20 14:54:20 +0200

    Stop no longer stops with fade

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -227,8 +227,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
 
             if ([asset isKindOfClass:[NativeAudioAsset class]]) {
                 NativeAudioAsset *_asset = (NativeAudioAsset*) asset;
-                // Music assets are faded out
-                [_asset stopWithFade];
+                [_asset stop];
 
                 NSString *RESULT = [NSString stringWithFormat:@"%@ (%@)", INFO_PLAYBACK_STOP, audioID];
                 [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: RESULT] callbackId:callbackId];
